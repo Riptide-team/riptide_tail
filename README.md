dccaaa1ead012b3b120c3de2f2295e701f0364b1
Teusner <quentin.brateau@ensta-bretagne.org>
2023-03-07 09:39:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Serial communication
+
+## Baudrate
+
+The baudrate is `115200` and the communication is operating over usb between arduino and Raspberry Pi.
+
+## Commands from raspberry
+
+### Command frame
+Commands from raspberry are sent using bytes. The line is finished by a `CR` `LF` (`\r\n`).
+
+### Frame content
+Bytes are hex values between 1000 and 2000 (coded on two consecutive bytes) which are direct us request on the output channel.
+Channel order is :
+
+- Thruster channel
+- D fin channel
+- P fin channel
+- S fin channel
+
+### Example
+
+All to neutral:
+`\0x05\0xDC\0x05\0xDC\0x05\0xDC\0x05\0xDC\r\n`
+
+Thruster full forward, fins full backward:
+`\0x07\0xD0\0x03\0xE8\0x03\0xE8\0x53\0xE8\r\n`
+
+## Commands from arduino
+
+### Command frame
+Commands from raspberry are sent using bytes. The line is finished by a `CR` `LF` (`\r\n`).
+
+### Frame content
+Bytes are hex values between 1000 and 2000 (on three hexadecimal digits) which are direct us request from the RC captured by arduino and real generated PWM to actuators.
+Channel order is :
+
+- Thruster channel
+- D fin channel
+- P fin channel
+- S fin channel
+- RC channel 1 (Manual button)
+- RC channel 2 (Timer button)
+- RC channel 3 (Thruster)
+- RC channel 4 (D fin)
+- RC channel 5 (P fin)
+- RC channel 6 (S fin)
+
+### Example
+
+All to neutral:
+`\0x05\0xDC\0x05\0xDC\0x05\0xDC\0x05\0xDC\0x05\0xDC\0x05\0xDC\0x05\0xDC\0x05\0xDC\0x05\0xDC\0x05\0xDC\r\n`
+
+Thruster full forward, fins full backward, in manual mode, with timer at the minimum, so the RC captured commands are commands applied to the actuators:
+`\0x07\0xD0\0x03\0xE8\0x03\0xE8\0x53\0xE8\0x07\0xD0\0x03\0xE8\0x07\0xD0\0x03\0xE8\0x03\0xE8\0x53\0xE8\r\n`
---
+++ src/main.ino
@@ -0,0 +1,123 @@
+#include <Servo.h>
+#include <PPMReader.h>
+
+// Loop delay
+#define LOOP_DELAY 20      // ms
+
+// Initialize a PPMReader on digital pin 3 with 6 expected channels.
+#define RC_PIN 3            // D3
+#define CHANNEL_AMOUNT 6    // number of PPM channel
+PPMReader ppm(RC_PIN, CHANNEL_AMOUNT);
+
+// Initialize actuators outputs
+#define THRUSTER_PIN 5      // D5
+#define D_FIN_PIN 6         // D6
+#define P_FIN_PIN 9         // D9
+#define S_FIN_PIN 10        // D10
+
+Servo thruster;
+Servo d_fin;
+Servo p_fin;
+Servo s_fin;
+
+// Manual mode threshold
+#define MANUAL_THRESHOLD 1750
+bool manual=false;
+
+// Temporary serial read buffer 
+uint8_t read_cursor = 0;
+uint8_t temporary_read_serial_buffer[10];
+
+// Read serial buffer
+uint8_t read_serial_buffer[8];
+
+// RC read buffer
+uint8_t read_rc_buffer[12];
+
+// Initialize serial read and write buffers
+uint8_t write_buffer[8];
+
+void write_actuators() {
+    thruster.writeMicroseconds(write_buffer[0]*255+write_buffer[1]);
+    d_fin.writeMicroseconds(write_buffer[2]*255+write_buffer[3]);
+    p_fin.writeMicroseconds(write_buffer[4]*255+write_buffer[5]);
+    s_fin.writeMicroseconds(write_buffer[6]*255+write_buffer[7]);
+}
+
+void setup() {
+    // Serial communication with raspberry
+    Serial.begin(115200);
+
+    // Servo creation
+    thruster.attach(THRUSTER_PIN);
+    d_fin.attach(D_FIN_PIN);
+    p_fin.attach(P_FIN_PIN);
+    s_fin.attach(S_FIN_PIN);
+
+    // PPM tuning
+    ppm.minChannelValue = 1000;     // (us)
+    ppm.maxChannelValue = 2000;     // (us)
+    ppm.channelValueMaxError = 40;  // (us)
+    ppm.blankTime = 2500;           // (us)
+
+    // Filling read buffers to neutral commands
+    for (uint8_t i=0; i<4; ++i) {
+        read_serial_buffer[2*i] = highByte(1500);
+        read_serial_buffer[2*i+1] = lowByte(1500);
+    }
+    for (uint8_t i=0; i<5; ++i) {
+        read_rc_buffer[2*i] = highByte(1500);
+        read_rc_buffer[2*i+1] = lowByte(1500);
+    }
+}
+
+void loop() {
+    // Reading PPM RC Receiver into write buffer
+    for (uint8_t channel = 0; channel < CHANNEL_AMOUNT; ++channel) {
+        uint16_t value = ppm.latestValidChannelValue(channel, 1500);
+        read_rc_buffer[2*channel] = highByte(value);
+        read_rc_buffer[2*channel+1] = lowByte(value);
+    }
+
+    // Reading commands from serial
+    while ((Serial.available()) and (read_cursor < 10)) {
+        // Reading incomming char
+        uint8_t inByte = Serial.read();
+        temporary_read_serial_buffer[read_cursor] = inByte;
+        read_cursor++;
+
+        // If a complete frame has been recevied
+        if (read_cursor > 9) {
+            // Put the read cursor at the beginning
+            read_cursor = 0;
+
+            // If the frame is finishing by "\r\n" then copying the temporary serial buffer to the read_serial_buffer
+            if ((temporary_read_serial_buffer[8] == "\r") and (temporary_read_serial_buffer[9] == "\n")) {
+                memcpy(read_serial_buffer, temporary_read_serial_buffer, 8);
+            }
+            break;
+        }
+    }
+
+    // Writing commands on actuators depending on the manual or automatic mode
+    uint16_t manual_value = read_rc_buffer[0]*255+read_rc_buffer[1];
+    if (manual_value > MANUAL_THRESHOLD) {
+        memcpy(write_buffer, read_rc_buffer+4, 8);
+    }
+    else {
+        memcpy(write_buffer, read_serial_buffer, 8);
+    }
+    write_actuators();
+
+    // Writing actuators state on the serial port
+    Serial.write(write_buffer, 8);
+
+    // Writing RC Receiver state
+    Serial.write(read_rc_buffer, 12);
+
+    // Serial new line
+    Serial.write("\r\n");
+
+    // Loop delay
+    delay(LOOP_DELAY);
+}
